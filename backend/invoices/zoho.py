6d3e62624dab026d76826bb3e836cdd03ccd8a56
Rayhan Islam <rayhanislam@Mac.modem>
2026-05-03 18:51:28 +1000

    fix refresh token test2

--- backend/invoices/zoho.py
+++ backend/invoices/zoho.py
@@ -10,11 +10,12 @@ ZOHO_ITEMS_URL = 'https://www.zohoapis.com.au/books/v3/items'
 # Step 1 — Generate the URL that redirects the user/to Zoho's login page to grant access
 def get_zoho_auth_url():
     params = {
-        'scope':'ZohoBooks.invoices.READ,ZohoBooks.invoices.CREATE,ZohoBooks.items.READ',
+        'scope': 'ZohoBooks.invoices.READ,ZohoBooks.invoices.CREATE,ZohoBooks.items.READ',
         'client_id': settings.ZOHO_CLIENT_ID,
         'response_type': 'code',
         'redirect_uri': 'https://alpha-tracking.onrender.com/api/zoho/callback/',
         'access_type': 'offline',
+        'prompt': 'consent',
     }
      # Build the URL with parameters
     param_string = '&'.join([f'{k}={v}' for k, v in params.items()])
